a47991654c74adb2724628ae414c961251cf1560
rashidbaset <rashid.baset@gmail.com>
2016-03-17 11:09:29 -0700

    added top 5 games and console log.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// var name = prompt ('What\'s your name?');
+ var name = prompt ('What\'s your name?');
 // alert('Hi, ' + name + ' welcome to Rashid 2.0. Please answer the following 5 questions. Please answer with either y/n or yes/no.');
-// var correctGuess = 0
+ var correctGuess = 0;
 //
 // var ques_location = prompt('I moved to Seattle from Oregon.');
 // ques_location = ques_location.toLowerCase();
@@ -9,10 +9,10 @@
 // }
 //   else if (ques_location === 'no' || ques_location === 'n'){
 //     alert('Correct!');
-//     correctGuess++
+//     correctGuess++;
 //   }
 //   else {
-//     alert('You did something weird')
+//     alert('You did something weird');
 //   }
 // console.log(name + ' answered ' + ques_location + ' for ques_location');
 //
@@ -23,10 +23,10 @@
 // }
 //   else if (ques_seattle === 'no' || ques_seattle === 'n'){
 //     alert ('Correct!');
-//     correctGuess++
+//     correctGuess++;
 //   }
 //   else {
-//     alert ('You did something weird')
+//     alert ('You did something weird');
 //   }
 // console.log(name + ' answered ' + ques_seattle + ' for ques_seattle');
 //
@@ -34,7 +34,7 @@
 // ques_interest = ques_interest.toLowerCase();
 // if (ques_interest === 'yes' || ques_interest === 'y'){
 //   alert('You are correct!');
-//   correctGuess++
+//   correctGuess++;
 // }
 //   else if (ques_interest === 'no' || ques_interest === 'n'){
 //     alert ('Incorrect');
@@ -48,7 +48,7 @@
 // ques_education = ques_education.toLowerCase();
 // if (ques_education === 'yes' || ques_education === 'y'){
 //   alert('You are correct!');
-//   correctGuess++
+//   correctGuess++;
 // }
 //   else if (ques_education === 'no' || ques_education === 'n'){
 //     alert ('Incorrect');
@@ -58,25 +58,29 @@
 //   }
 // console.log(name + ' answered ' + ques_education + ' for ques_education');
 //
-// var ques_goal = prompt('My goal is to get a job as a software engineer.')
+// var ques_goal = prompt('My goal is to get a job as a software engineer.');
 // ques_goal = ques_goal.toLowerCase();
 // if (ques_goal === 'yes' || ques_goal === 'y'){
-//   console.log(name + 'answered yes to ques_goal');
-//   correctGuess++
+//   console.log(name + ' answered yes to ques_goal');
+//   correctGuess++;
 //   var ques_goal_two = prompt('Correct! For a civic tech company?');
 //   ques_goal_two = ques_goal_two.toLowerCase();
 //   if (ques_goal_two === 'yes' || ques_goal_two === 'y'){
-//     alert('You are correct!')
-//     correctGuess++
+//     console.log(name + ' correctly guessed ques_goal_two')
+//     alert('You are correct!');
+//     correctGuess++;
 //   } else if (ques_goal_two === 'n' || ques_goal_two === 'no') {
 //     alert('Incorrect');
+//     console.log(name + ' incorrectly guessed ques_goal_two')
 //   } else {
-//     alert('smh')
+//     alert('smh');
 //   }
 // } else if (ques_goal === 'no' || ques_goal === 'n'){
 //   alert ('Incorrect');
+//   console.log(name + ' incorrectly guessed ques_goal')
 // } else {
-//   alert ('You did something weird')
+//   alert ('You did something weird');
+//   console.log('Something weird happened for ' + name + ' on ques_goal');
 // }
 // console.log(name + ' answered ' + ques_goal + ' for ques_goal');
 //
@@ -85,21 +89,41 @@
 //   var ques_topten = prompt('With regard to the top-ten list, guess the number corresponding to my favorite app. You have four tries!')
 //   ques_topten = parseInt(ques_topten);
 //   if(ques_topten === 2){
+//     console.log(name + ' guessed ques_topten correct')
 //     alert('Correct!');
-//     correctGuess++
+//     correctGuess++;
 //     i = 4;
 //   } else if (ques_topten > 2){
-//     alert('Too high!')
-//     i++
+//     alert('Too high!');
+//     i++;
 //   } else if (ques_topten < 2){
-//     alert ('Too low!')
+//     alert ('Too low!');
 //     i++
 //   } else if (isNaN(ques_topten) === true){
-//     alert('Please enter a number!')
+//     alert('Please enter a number!');
+//     console.log(name + ' entered a NaN for ques_topten');
 //     i++;
 //   }
 // };
+//
+// console.log(name + ' answered the following questions correct out of 8, ' + correctGuess);
+// alert('You answered ' + correctGuess + ' questions correct out of 8!');
 
-//alert('You answered ' + correctGuess + ' questions correct out of 8!');
-//var userFavoriteGames = prompt('What are your top 5 favorite games?');
-//alert('Your top 5 are: ' + userFavoriteGames);
+ confirm('Enter your Top 5 Favorite Games');
+ var arrayGame = [];
+ var game5 = prompt('Number 5?');
+ console.log(name + ' entered the following for 5th game, ' + game5);
+ arrayGame.push(game5);
+ var game4 = prompt('Number 4?');
+ console.log(name + ' entered the following for 4th game, ' + game4);
+ arrayGame.push(game4);
+ var game3 = prompt('Number 3?');
+ console.log(name + ' entered the following for 3rd game, ' + game3);
+ arrayGame.push(game3);
+ var game2 = prompt('Number 2?');
+ console.log(name + ' entered the following for 2nd game, ' + game2);
+ arrayGame.push(game2);
+ var game1 = prompt('Favorite game?');
+ arrayGame.push(game1);
+ console.log(name + ' entered the following for favorite game, ' + game1);
+ console.log(name + '\'s top 5 games are: ' + arrayGame);
